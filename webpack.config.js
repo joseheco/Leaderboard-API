2a210383ec74e9f5c1004e3289c38edac655314d
Jose Herrera <Heco@Alejandras-MacBook-Pro.local>
2022-01-27 19:35:43 -0500

    fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ruleForStyles = {
   test: /\.css$/,
-  use: ['style-loader', 'css-loader']
-}
+  use: ['style-loader', 'css-loader'],
+};
 
 const ruleForJS = {
   test: /\.js$/,
   loader: 'babel-loader',
-}
+};
 
-const rules = [ruleForJS, ruleForStyles]
+const rules = [ruleForJS, ruleForStyles];
 
 module.exports = {
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' })
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
   ],
-  module: { rules } 
-}
+  module: { rules },
+};
