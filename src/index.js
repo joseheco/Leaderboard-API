ad9bbd523b667d5142ad8fd30663e63521bf0206
Jose Herrera <Heco@Alejandras-MacBook-Pro.local>
2022-01-29 00:16:45 -0500

    add Js modules ES6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './styles.css';
+import { name, score, submitBtn, refreshBtn } from './utils.js'
+import { sendData } from './sendData.js'
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zsarJ95NcOEnRbhOla89/scores/'
+
+submitBtn.addEventListener('click', async () => {
+  event.preventDefault();
+  await sendData( url , { user: name.value, score: score.value });
+  name.value = '';
+  score.value = '';
+  const msg = document.getElementById('msg');
+  setTimeout(() => { msg.innerHTML = 'Score added successfully'}, 1000);
+  setTimeout(() => { msg.innerHTML = ''}, 4000);
+});
+
+const scoresLi = document.getElementsByClassName('boardList')[0];
+
+const loadList = async () => {
+  scoresLi.innerHTML = '';
+  const { result: scores } = await fetch(url)
+  .then((resp) => resp.json());
+  scores.forEach((result) => {
+    const list = document.createElement('li');
+    list.innerHTML = `${result.user} : ${result.score}`;
+    scoresLi.appendChild(list);
+  });
+};
+
+refreshBtn.addEventListener('click', loadList);
+window.onload = loadList;
---
+++ src/sendData.js
@@ -0,0 +1,17 @@
+const sendData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+export { sendData }
